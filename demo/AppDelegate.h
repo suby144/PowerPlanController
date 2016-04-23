b4fe71e923c5c0bb27fbda17c406c2e6fb6a96ac
suby sukumaran <suby1729@gmail.com>
2016-04-23 18:18:47 +0530

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by Appcircle on 4/23/16.
+//  Copyright © 2016 Appcircle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
